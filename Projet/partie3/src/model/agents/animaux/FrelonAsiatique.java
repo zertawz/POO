501222b1beced61f14fcbc388319e7d530dd0527
Jean-Christophe-BURNOT <jean-christophe.burnot@cpe.fr>
2023-01-08 09:57:09 +0100

    Ajout ressources et checkout section8

---
+++ Projet/partie3/src/model/agents/animaux/FrelonAsiatique.java
@@ -0,0 +1,17 @@
+package model.agents.animaux;
+
+import java.awt.Point;
+
+import model.agents.Sexe;
+
+public class FrelonAsiatique extends Frelon{
+
+	public FrelonAsiatique(Sexe s, Point p) {
+		/*
+		 * TODO
+		 */
+	}
+
+	
+
+}
---
+++ Projet/partie3/src/model/decor/Champ.java
@@ -0,0 +1,12 @@
+package model.decor;
+
+import java.awt.Point;
+
+public class Champ extends Decor {
+
+	public Champ(Point p) {
+		super(p);
+		// TODO Auto-generated constructor stub
+	}
+
+}
